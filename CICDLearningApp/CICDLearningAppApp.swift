55e1148d8f1ca14b4c7b07e558c8243a0fe9847b
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-05-27 08:31:01 +0530

    Initial Commit

---
+++ CICDLearningApp/CICDLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDLearningAppApp.swift
+//  CICDLearningApp
+//
+//  Created by Adarsh Ranjan on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
